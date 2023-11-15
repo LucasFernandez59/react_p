79691455adf08436dd7963bae82c57a03483bbb7
LucasF <che.fernandez255@gmail.com>
2023-11-14 20:57:36 -0700

    netlify test

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './syles.css'
 
 import App from './App';
 import AboutPage from './components/Pages/AboutPage';
